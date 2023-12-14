302e477af2b7ee4661c75842245fbc715f61c947
AryanVatsayan <136555025+AryanVatsayan@users.noreply.github.com>
2023-12-14 13:38:35 +0000

    3rd commit

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,19 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2">1);  // true bcz js automatically converts the "2" into integer type
+// console.log("02">1); // true bcz js automatically converts the "02" into integer type
+
+// console.log(null > 0 );  //false
+// console.log(null == 0 ); //false
+// console.log(null >= 0);  //true bcz == check ,but comparison operators convert null into an integer i.e. 0 thus its output is 0
+
+// console.log(undefined == 0);  //false
+// console.log(undefined > 0);   //false
+// console.log(undefined < 0);   //false
+
+// === (strict check)
+console.log("2" === 2); //it doesn't convert the value of "2" into int but only compares
