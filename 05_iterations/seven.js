95642c88d7c1c229ce6c8dfdb3dcf0936501e0c1
AryanVatsayan <136555025+AryanVatsayan@users.noreply.github.com>
2023-12-25 07:24:27 +0000

    commit

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,3 +1,12 @@
 const myNumers = [1,2,3,4,5,6,7,8,9,10]
-const newNums = myNumers.map((num) => num+10)
+// const newNums = myNumers.map((num) => num + 10)
+
+//chaining in js
+//chaining is a process in which the multiple methods are applied on the same datatype
+
+const newNums = myNumers
+                .map((nums) => nums * 10 )
+                .map((nums) => nums +1 )
+                .filter((nums) => nums >= 40)
+                
 console.log(newNums);
